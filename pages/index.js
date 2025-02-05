58f76163cb8df49211227272ca82f983cde0ad65
Matheus_Luiz <86157656+M4theuslm@users.noreply.github.com>
2025-02-05 02:19:37 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Adriana, Eu te amo ❤️</h1>
+  return <h1>Adriana, Eu te amo ❤︝</h1>;
+}
+
+function Teste() {
+  console.log("Teste");
+}
+
+function Teste2() {
+  console.log("identa��o errada");
 }
 
 export default Home;
