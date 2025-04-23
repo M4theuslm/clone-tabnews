1d029a101ba503d01f82d2e4885a20cad961f525
Matheus_Luiz <86157656+M4theuslm@users.noreply.github.com>
2025-04-23 01:48:21 +0000

    add `api//v1/status` endpoint

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,5 @@
 function Home() {
-  return <h1>Adriana, Eu te amo ‚ù§Ô∏ù</h1>;
-}
-
-function Teste() {
-  console.log("Teste");
-}
-
-function Teste2() {
-  console.log("identaÁ„o errada");
+  return <h1>Adriana, Eu te amo.</h1>;
 }
 
 export default Home;
