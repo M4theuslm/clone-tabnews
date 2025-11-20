92b47cf1a91a5c3f64ae5d7547b5f6ac1efd63f5
Matheus_Luiz <86157656+M4theuslm@users.noreply.github.com>
2025-11-20 16:15:17 +0000

    add `database.js` e `.env`files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
+import database from "../../../../infra/database.js";
 
-function status(request, response) {
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1 AS SUM;');
+  console.log(result.rows);
   response.status(200).json({ "chave":"são acima da média."});
 }
 
